6d6dff214a9096b329db89aa619674222adb4efa
Audrone <81379683+audramvc@users.noreply.github.com>
2025-03-09 13:47:22 -0400

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl:"https://automationexercise.com",
-    //specPattern: "cypress/e2e/**/*.cy.js",
+    specPattern: "cypress/e2e/**/*.cy.js",
     supportFile: "cypress/support/e2e.js", 
    // env: {
         //admin_email: "admin@automationexercise.com",
